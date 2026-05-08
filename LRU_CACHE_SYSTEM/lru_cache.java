b44481e82489453f1db2bee26b6e36cface1d6aa
Arpan152023 <kumariarpan602@gmail.com>
2026-05-08 19:20:23 +0530

    Implement LRUCache class with get and put methods

---
+++ LRU_CACHE_SYSTEM/lru_cache.java
@@ -0,0 +1,73 @@
+class LRUCache {
+    class Node {
+        int key;
+        int val;
+        Node prev;
+        Node next;
+
+        Node(int key, int val) {
+            this.key = key;
+            this.val = val;
+        }
+    }
+    Node head = new Node(-1, -1);
+    Node tail = new Node(-1, -1);
+
+    HashMap<Integer, Node> m = new HashMap<>();
+    int cap;
+    public LRUCache(int capacity) {
+        cap = capacity;
+        head.next = tail;
+        tail.prev = head;
+    }
+    void addNode(Node newNode) {
+        Node nextNode = head.next;
+        newNode.next = nextNode;
+        newNode.prev = head;
+        head.next = newNode;
+        nextNode.prev = newNode;
+    }
+    void deleteNode(Node oldNode) {
+        Node oldPrev = oldNode.prev;
+        Node oldNext = oldNode.next;
+        oldPrev.next = oldNext;
+        oldNext.prev = oldPrev;
+    }
+    public int get(int key) {
+        if (m.containsKey(key)) {
+            Node ansNode = m.get(key);
+            int ans = ansNode.val;
+            m.remove(key);
+            deleteNode(ansNode);
+            addNode(ansNode);
+
+            m.put(key, ansNode);
+            return ans;
+        }
+        return -1;
+    }
+    public void put(int key, int value) {
+        if (m.containsKey(key)) {
+            Node oldNode = m.get(key);
+            m.remove(key);           //map re remove
+            deleteNode(oldNode);     //node se remove
+        }
+        if (m.size() == cap) {
+            m.remove(tail.prev.key);   //map re remove
+            deleteNode(tail.prev);     //node se remove
+        }
+        Node newNode = new Node(key, value);
+        addNode(newNode);
+        m.put(key, newNode);
+    }
+}
+
+
+
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache obj = new LRUCache(capacity);
+ * int param_1 = obj.get(key);
+ * obj.put(key,value);
+ */
