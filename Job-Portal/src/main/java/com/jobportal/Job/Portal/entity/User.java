a101ef1f86a11429d35107e4db429ed7db5bf35b
Arpan152023 <kumariarpan602@gmail.com>
2026-05-09 18:23:19 +0530

    job portal project added

---
+++ Job-Portal/src/main/java/com/jobportal/Job/Portal/entity/User.java
@@ -0,0 +1,20 @@
+package com.jobportal.Job.Portal.entity;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import com.jobportal.Job.Portal.dto.AccountType;
+import lombok.Data;
+
+@Data
+@Document(collection="users")
+public class User {
+    @Id
+    private String id;
+    private String name;
+    @Indexed(unique = true)
+    private String email;
+    private String password;
+    private AccountType accountType;
+}
